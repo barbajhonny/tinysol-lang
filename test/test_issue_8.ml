a0ee25031eed88329185678185499d60dbb89552
Lorenzo Derosas <eic17h+github@gmail.com>
2026-02-15 17:40:02 +0100

    Issue #8: test

--- test/test_issue_8.ml
+++ test/test_issue_8.ml
@@ -1,36 +1,60 @@
 
 open Typechecker
 
-(* test di Bartoletti *)
+let%test "test_typecheck_constant_nostri_1" = test_typecheck
+  "contract C {
+    int constant N;
+    constructor() { N = 3; } 
+    function f(int n) external { N = 2; }
+  }"
+  false
 
-let%test "test_typecheck_constant_1" = test_typecheck
+let%test "test_typecheck_constant_nostri_2" = test_typecheck
   "contract C {
     int constant N=1;
-    constructor() { } 
-    function f(int n) external { }
+    constructor() {} 
+    function f(int n) external {}
   }"
   true
 
-let%test "test_typecheck_constant_2" = test_typecheck
+(* altro tipo di assegnazione *)
+let%test "test_typecheck_constant_nostri_3" = test_typecheck
   "contract C {
     int constant N=1;
-    constructor() { } 
-    function f(int n) external { N=2; }
+    constructor() { N += 2;} 
+    function f(int n) external { N -= 3;}
   }"
   false
 
-let%test "test_typecheck_constant_3" = test_typecheck
+(* altro tipo di variabile *)
+let%test "test_typecheck_constant_nostri_4" = test_typecheck
   "contract C {
-    int constant N=1;
-    constructor() { N=2; } 
-    function f(int n) external { }
+    bool constant N=true;
+    constructor() {} 
+    function f(int n) external {}
+  }"
+  true
+
+let%test "test_typecheck_constant_nostri_5" = test_typecheck
+  "contract C {
+    bool constant N;
+    constructor() {} 
+    function f(int n) external {}
   }"
   false
 
-let%test "test_typecheck_constant_4" = test_typecheck
+let%test "test_typecheck_constant_nostri_6" = test_typecheck
   "contract C {
-    int constant N;
-    constructor() { } 
-    function f(int n) external { }
+    bool constant N=true;
+    constructor() {N = false;} 
+    function f(int n) external {}
+  }"
+  false
+
+let%test "test_typecheck_constant_nostri_7" = test_typecheck
+  "contract C {
+    bool constant N=true;
+    constructor() {} 
+    function f(int n) external {N = true;}
   }"
   false
