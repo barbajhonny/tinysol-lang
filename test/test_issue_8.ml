b95fe4a18d801ad2df921700d483523dfd8417d9
barbajhonny <giovanniscano52@gmail.com>
2026-02-16 12:09:05 +0100

    fix test_issue_8

--- test/test_issue_8.ml
+++ test/test_issue_8.ml
@@ -1,90 +1,5 @@
 open Typechecker
 
-(*  true perchè la costante è dichiarata secondo i criteri (dichiarazione + assegnamento)  *)
-let%test "test_typecheck_constant_nostri_2" = test_typecheck
-  "contract C {
-    int constant N=1;
-    constructor() {} 
-    function f(int n) external {}
-  }"
-  true
-
-
-(*  false perchè si prova a riassegnare la costante *)
-let%test "test_typecheck_constant_nostri_2a" = test_typecheck
-  "contract C {
-    int constant N=1;
-    constructor() {} 
-    function f(int n) external { N = 2;}
-  }"
-  false
-
-
-(*  false perchè non è concesso non inizializzare una costante  *)
-let%test "test_typecheck_constant_nostri_1" = test_typecheck
-  "contract C {
-    int constant N;
-    constructor() { N = 3; } 
-    function f(int n) external { N = 2; }
-  }"
-  false
-
-
-(* false perchè non è concesso riassegnare una costante *)
-let%test "test_typecheck_constant_nostri_3" = test_typecheck
-  "contract C {
-    int constant N=1;
-    constructor() { N += 2;} 
-    function f(int n) external { N -= 3;}
-  }"
-  false
-
-
-(* altro tipo di variabile *)
-let%test "test_typecheck_constant_nostri_4" = test_typecheck
-  "contract C {
-    bool constant N=true;
-    constructor() {} 
-    function f(int n) external {}
-  }"
-  true
-
-let%test "test_typecheck_constant_nostri_5" = test_typecheck
-  "contract C {
-    bool constant N;
-    constructor() {} 
-    function f(int n) external {}
-  }"
-  false
-
-(*false non è concesso riassegnare una costante*)
-let%test "test_typecheck_constant_nostri_6" = test_typecheck
-  "contract C {
-    bool constant N=true;
-    constructor() {N = false;} 
-    function f(int n) external {}
-  }"
-  false
-
-
-let%test "test_typecheck_constant_nostri_7" = test_typecheck
-  "contract C {
-    bool constant N=true;
-    constructor() {} 
-    function f(int n) external {N = true;}
-  }"
-  false
-
-  (*NON SAPPIAMO SE LASCIARLO O MENO*)
-let%test "test_issue_11_strano" = test_typecheck
-  "contract C {
-    int constant N=1;
-    int x=1;
-    function f(int n) external { if (x==2) x+=1; else N=0; }
-  }"
-  false
-
-
 (* === TEST NUOVI PIU' ORDINATI? === *)
 
 (* Costante int usata correttamente *)
@@ -97,7 +12,7 @@ let%test "test_constant_int_ok" = test_typecheck
   true
 
 (* Costante bool usata correttamente *)
-let%test "test_constant_int_ok" = test_typecheck
+let%test "test_constant_bool_ok" = test_typecheck
   "contract C {
     bool constant N=true;
     constructor() {} 
