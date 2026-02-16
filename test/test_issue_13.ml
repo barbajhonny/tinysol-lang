16743b59625c150242b5a817d06008d69eb7a2b8
barbajhonny <giovanniscano52@gmail.com>
2026-02-16 20:02:49 +0100

    riga 73 test_issue_13.ml

--- test/test_issue_13.ml
+++ test/test_issue_13.ml
@@ -1,28 +1,28 @@
 open Typechecker
 open Semantics
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_true" = test_typecheck
   "contract C {
       int x;
       function f(int y) public returns(int) { x = y * ((1 / 3) * 6); }
 }"
 true
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_false1" = test_typecheck
   "contract C {
       int x;
       function f(int y) public returns(int) { x = 2 * ((1 / 0) * 6); }
 }"
 false
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_false2" = test_typecheck
   "contract C {
       int x;
       function f(int y) public returns(int) { x = (1 / 0); }
 }"
 false
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_false3" = test_typecheck
   "contract C {
       int x;
 int y = 2;
@@ -31,7 +31,7 @@ int y = 2;
 false
 
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_true2" = test_typecheck
   "contract C {
       int x;
       int y = 2;
@@ -41,7 +41,7 @@ int z= 0;
 true
 
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_false4" = test_typecheck
   "contract C {
       int x;
       uint y = 2;
@@ -50,7 +50,7 @@ let%test "test_constant_int_if_false" = test_typecheck
 false
 
 
-let%test "test_constant_int_if_false" = test_typecheck
+let%test "test_constant_int_if_true3" = test_typecheck
   "contract C {
       int x=2;
       int y = 2;
@@ -60,7 +60,7 @@ true
 
 
 
-  let%test "test_constant_int_if_false" = test_typecheck
+  let%test "test_constant_int_if_true4" = test_typecheck
    "contract C {
       int x=5;
       int y = 2;
@@ -68,6 +68,26 @@ true
       function f() public returns(int) { x = (4/2); }
 }"
 true
+
+(*----------chiedere qua speigazion ?????????????????????*)
+  let%test "test_constant_int_if_true4" = test_typecheck
+   "contract C {
+      uint x= 5.98 * 100;
+      int y = 2;
+      int z= 4;
+      function f() public returns(int) { y = x/z; }
+}"
+true
+
+
+let%test "test_constant_int_if_true5" = test_typecheck
+  "contract C {
+      bool x;
+      bool y = true;
+      int z = 5;
+      function f(int y) public returns(int) { x = (y / z); }
+}"
+false
 
 
 
